57edd05fb0079cf88a299dab723c7d4c2cb5f997
Olivia Waithira <oliviawaithira7@gmail.com>
2024-07-23 01:34:04 +0300

    javascript

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const btnE1 = document.getElementById("btn")
-
+const errormessage
 
 function fetchimage(){
-  fetch() 
+    const inputValue = document.getElementById("input").value;
+
+    if(inputValue > 10 || inputValue < 1){
+
+    }
+  fetch(`https://api.unsplash.com/photos?per_page=${inputValue}&page=1&client_id=5H2wZWtiq6G4_7CM4qqbS2yHw8CdUDXJvlf3q0pDqnw`).then((res)=>res.json().then((data)=>{
+    console.log(data);
+  })
+ );
 }
 
 btnE1.addEventListener("click", fetchimage)
+
