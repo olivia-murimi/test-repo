c131b13f4767bf9cfa3046d1d063ff63de019bd3
Olivia Waithira <oliviawaithira7@gmail.com>
2024-07-24 14:34:48 +0300

    photo gallery

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 const btnE1 = document.getElementById("btn")
 const errormessagee1 = document.getElementById("errormessage")
-
+ const galleryE1 = document.getElementById("gallery")
  async function fetchimage(){
     const inputValue = document.getElementById("input").value;
 
     if(inputValue > 10 || inputValue < 1){
      errormessagee1.style.display = "block";
      errormessageE1.innerText = "Number should be between 0 and 11"
-     return
+     return;
     }
+     
+    imgs = "";
+
     try{
-      await fetch(`https://api.unsplash.com/photos?per_page=${inputValue}&page=1&{Math.round (Math.random * 1000
+      await fetch(`https://api.unsplash.com/photos?per_page=${inputValue}&page=1&{Math.round (Math.random() * 1000
+
         )}&
         client_id=5H2wZWtiq6G4_7CM4qqbS2yHw8CdUDXJvlf3q0pDqnw`
 
-      ).then((res)=>res.json().then((data)=>{
-        console.log(data);
+      ).then((res)=> 
+        res.json().then((data)=>{
+        if(data){
+          data.forEach((pic)=>{
+           imgs +=`
+           <img src=${pic.urls.small} alt="image"/>
+           `;
+           galleryE1.innerHTML = imgs;
+          })
+        }
       })
      ); 
      errormessageE1.style.dispaly = "none"
