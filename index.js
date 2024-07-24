cc643d1677f6ae8724c42dec239dcae82f2c8c83
Olivia Waithira <oliviawaithira7@gmail.com>
2024-07-25 00:18:24 +0300

    photo gallery

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const btnE1 = document.getElementById("btn")
-const errormessagee1 = document.getElementById("errormessage")
+const errormessageE1 = document.getElementById("errormessage")
  const galleryE1 = document.getElementById("gallery")
  async function fetchimage(){
     const inputValue = document.getElementById("input").value;
 
-    if(inputValue > 10 || inputValue < 1){
-     errormessagee1.style.display = "block";
+    if(inputValue > 10 || inputValue < 1)
+      {
+     errormessageE1.style.display = "block";
      errormessageE1.innerText = "Number should be between 0 and 11"
      return;
     }
@@ -21,22 +22,25 @@ const errormessagee1 = document.getElementById("errormessage")
       ).then((res)=> 
         res.json().then((data)=>{
         if(data){
-          data.forEach((pic)=>{
-           imgs +=`
+          data.forEach((pic) => {
+           imgs += `
            <img src=${pic.urls.small} alt="image"/>
            `;
-           galleryE1.innerHTML = imgs;
-          })
+           galleryE1.style.display = "block";
+           galleryE1.innerHTML = imgs ;
+          });
+          
         }
       })
      ); 
      errormessageE1.style.dispaly = "none"
     }catch (error){
       errormessageE1.style.display = "block";
-      errormessageE1.innerHTML = "An error happened, try again later"
+      errormessageE1.innerHTML = "An error happened, try again later";
     }
   
 }
 
 btnE1.addEventListener("click", fetchimage)
+
 
