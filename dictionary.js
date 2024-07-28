d58657e3d613fbe5f0f6dc65dcd95e075412fd41
Olivia Waithira <oliviawaithira7@gmail.com>
2024-07-29 02:00:58 +0300

    adding words

--- dictionary.js
+++ dictionary.js
@@ -2,8 +2,12 @@ const inputEl = document.getElementById("input")
 
 
 
-
+function fetchAPI(word){
+ const
+}
 inputEl.addEventListener("keyup", (e)=>{
-
+ if(e.target.value && e.key === "Enter"){
+    fetchAPI(e.target.value)
+ }
 
 })
