4f8670f951a582e3a4c6291fef3df3bba06ef3aa
Olivia Waithira <oliviawaithira7@gmail.com>
2024-07-30 00:19:00 +0300

    dictionary

--- dictionary.js
+++ dictionary.js
@@ -1,8 +1,10 @@
 const inputEl = document.getElementById("input")
-
+const infoTextEl = document.getElementById ("info-text")
 async function fetchAPI(word){
 
 try {
+  
+  infoTextE1.innerText = `Searching the meaning of "${word}"`
     const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
   const result = await fetch(url).then((res) => res.jon ());
 } catch (error) {
@@ -15,5 +17,6 @@ inputEl.addEventListener("keyup", (e)=>{
  }
 
 });
+
 
 
