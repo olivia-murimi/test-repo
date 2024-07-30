54f3e0e1d12d7474e406e6ff70275396dba2e3e9
Olivia Waithira <oliviawaithira7@gmail.com>
2024-07-31 00:21:37 +0300

    adding audio

--- dictionary.js
+++ dictionary.js
@@ -3,9 +3,10 @@ const infoTextEl = document.getElementById ("info-text")
 const meaningContainerEl = document.getElementById("meaning-container")
 const titleEl = document.getElementById("title")
 const meaningEl = document.getElementById("meaning")
+const audioEl = document.getElementById("audio")
+
 
 async function fetchAPI(word){
-
 try {
    infoTextEl.style.display ="block";
    meaningContainerEl.style.display = "none";
@@ -17,9 +18,12 @@ try {
   infoTextEl.style.display = "none"
   meaningContainerEl.style.display = "block";
    titleEl.innerText = result[0].word
+   meaningEl.innerText = result[0].meanings[0].definitions[0].definition;
+   audioEl.src = result[0].phonetics[0].audio
 } catch (error) {
   console.log(error);
  
+ }
 }
 inputEl.addEventListener("keyup", (e)=>{
  if(e.target.value && e.key === "Enter"){
@@ -28,6 +32,7 @@ inputEl.addEventListener("keyup", (e)=>{
 
 });
 
-}
+
+
 
 
