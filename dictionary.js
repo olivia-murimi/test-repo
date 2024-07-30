cfe2dee33e1af7d46e75001fe387e138448f74de
Olivia Waithira <oliviawaithira7@gmail.com>
2024-07-31 00:32:48 +0300

    adding things

--- dictionary.js
+++ dictionary.js
@@ -14,23 +14,30 @@ try {
     const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
   const result = await fetch(url).then((res) => res.jon ());
 
-
-  infoTextEl.style.display = "none"
+   if(result.title){
+    titleEl.innerText = word
+    meaningEl.innerText = "N/A";
+    audioEl.style.display = "none";
+   }else{
+    infoTextEl.style.display = "none"
   meaningContainerEl.style.display = "block";
    titleEl.innerText = result[0].word
    meaningEl.innerText = result[0].meanings[0].definitions[0].definition;
-   audioEl.src = result[0].phonetics[0].audio
+   audioEl.src = result[0].phonetics[0].audio;
+   }
 } catch (error) {
   console.log(error);
  
  }
 }
+
 inputEl.addEventListener("keyup", (e)=>{
  if(e.target.value && e.key === "Enter"){
     fetchAPI(e.target.value)
  }
 
 });
+
 
 
 
