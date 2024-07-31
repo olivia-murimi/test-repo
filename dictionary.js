dab2363bb9830f8bc980d81e98b6e6b3253be0da
Olivia Waithira <oliviawaithira7@gmail.com>
2024-08-01 00:28:25 +0300

    rectifying

--- dictionary.js
+++ dictionary.js
@@ -11,25 +11,25 @@ try {
    meaningContainerEl.style.display = "none";
   infoTextEl.innerText = `Searching the meaning of "${word}"`
     const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
-  const result = await fetch(url).then((res) => res.jon ());
+  const result = await fetch(url).then((res) => res.json ());
 
    if(result.title){
-    meaningContainerEl.style.display = "block"
+    meaningContainerEl.style.display = "block";
     infoTextEl.style.display = "none";
-    titleEl.innerText = word
+    titleEl.innerText = word;
     meaningEl.innerText = "N/A";
     audioEl.style.display = "none";
    } else{
-    infoTextEl.style.display = "none"
+    infoTextEl.style.display = "none";
   meaningContainerEl.style.display = "block";
-   audio.style.display = "inline-flex"
-   titleEl.innerText = result[0].word
+   audio.style.display = "inline-flex";
+   titleEl.innerText = result[0].word;
    meaningEl.innerText = result[0].meanings[0].definitions[0].definition;
    audioEl.src = result[0].phonetics[0].audio;
    }
 } catch (error) {
   console.log(error);
-  infoTextEl.innerText = `an error happened try again later`;
+  `infoTextEl.innerText = 'an error happened try again later';`
  }
 }
 
